f4e9f39a4566561e87f627cb6e143f88f89af213
Thiago de Oliveira <thiagos2000@gmail.com>
2021-07-12 21:01:44 -0300

    Criando a classe Player

---
+++ Mini1_InfiniteRunner/Entities/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  Mini1_InfiniteRunner
+//
+//  Created by Team 10 on 12/07/21.
+//
+
+import SpriteKit
+
+class Player {
+    // Propriedades
+    
+    // Inicializadores
+    
+    // Methodos
+}
