0fb865995e41f3b0cf39598911b9d8155e8c322f
vinicius <vini10b@hotmail.com>
2021-07-27 17:18:59 -0300

    arrumando a zposition do hud

--- Mini1_InfiniteRunner/Core/Extensions/GameScene+HUD.swift
+++ Mini1_InfiniteRunner/Core/Extensions/GameScene+HUD.swift
@@ -18,7 +18,7 @@ extension GameScene {
     func HUDCameraPosition() {
         countLabel.fontName = "Early GameBoy"
         countLabel.position = CGPoint(x: frame.size.width*(0.4), y: frame.size.height*(0.34))
-        countLabel.zPosition = 4
+        countLabel.zPosition = 100000
         countLabel.horizontalAlignmentMode = .left
         
         pumpkinHUD.position = CGPoint(x: frame.size.width*(0.35), y: frame.size.height*(0.38))
@@ -30,6 +30,7 @@ extension GameScene {
         //pumpkinHUD.setScale(2.5)
         pumpkinHUD.size = CGSize(width: 50, height: 50)
         pumpkinHUD.texture?.filteringMode = .nearest
+        pumpkinHUD.zPosition = 100000
         cameraNode.addChild(pumpkinHUD)
         
     }
@@ -40,7 +41,7 @@ extension GameScene {
 //        pauseButton.position = CGPoint(x: frame.size.width*0.1, y: frame.size.height*1.3)
         pauseButton.position = CGPoint(x: -350, y: 148)//x -350
         pauseButton.size = CGSize(width: 50, height: 50)
-        pauseButton.zPosition = 100
+        pauseButton.zPosition = 100000
         pauseButton.texture?.filteringMode = .nearest
         cameraNode.addChild(pauseButton)
     }
